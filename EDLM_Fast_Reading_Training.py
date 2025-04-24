4f3c74f3fd0315e3b2d002e3a5f0575fe281a607
JuanCastillo <juanecastilloch2@gmail.com>
2025-04-24 16:37:08 -0500

    final

--- EDLM_Fast_Reading_Training.py
+++ EDLM_Fast_Reading_Training.py
@@ -4,6 +4,7 @@ import os,csv,time
 
 def intro():
     os.system("clear")
+    os.system("explorer.exe .")
     print("\n#   INTRO   #")
     print("This program was designed as a tool for Speed Reading. In order to do so it")
     print("reads a TXT file on UTF format and print it by lines or chunks of a line")
@@ -23,9 +24,10 @@ def _fileByParragraph(book,line__):
         file1 = csv.reader(file0)
         for i in file1:
             _count +=1
-            if line__ <= _count:
+            if int(line__) <= _count:
                 #print(i)
                 if i == []:
+                    time.sleep(3)
                     os.system("clear")
                 else:
                     a=0    # List element counter
@@ -37,9 +39,9 @@ def _fileByParragraph(book,line__):
                         else:
                             _line = _line + ", " + i[a]
                         if len(i[a]) > 60:
-                            _time =_time  + 4
+                            _time =_time  + 5
                         elif len(i[a]) > 30:
-                            _time =_time + 3
+                            _time =_time + 4
                         else:
                              _time =_time + 1
                         a+=1
@@ -61,7 +63,7 @@ def _fileLineByLine(book,line__):
             if line__ <= _count:
                 #print(i)
                 if i == []:
-                    time.sleep(1)
+                    time.sleep(3)
                     os.system("clear")
                 else:
                     a=0    # List element counter
@@ -70,23 +72,57 @@ def _fileLineByLine(book,line__):
                     while a <= len(i)-1:
                         print(i[a])
                         if len(i[a]) > 60:
-                            _time =_time  +4
+                            _time =_time  + 5
                         elif len(i[a]) > 30:
-                            _time =_time + 3
+                            _time =_time + 4
                         else:
-                             _time =_time + 1
+                             _time =_time + 3
                         a+=1
                     print(_line)
                     time.sleep(_time)
                     _line, _time = 0, 0
 
+def _fileNormalReading(book,line__):
+    """
+    Get a text book and a line where to start presenting the lines
+    """
+    _count = 0    # Line counter
+    with open(book) as file0:
+        file1 = csv.reader(file0)
+        os.system("clear")
+        for i in file1:
+            _count +=1
+            linea =""
+            if i == []:
+                os.system("clear")
+            elif int(line__) <= _count:
+                if len(i) == 1:
+                    linea = i[0]
+                elif len(i) > 1:
+                    for row in range(len(i)):
+                        if row != len(i)-1:
+                            linea = linea + i[row]  + ","
+                        else: linea = linea + i[row]
+                print(f'[{_count}] ', linea , end = " ")
+                input()
+
 
 if __name__ == "__main__":
     intro()
     book = input("Input text file for reading: ")
-    line__ = int(input("Input text line number [0 = File Begin]: "))
-    opcion = input("Read text by Parragraph ( p ) / line( l ) : ")
+    default = 0
+    line__ = input("Input text line number [0 = File Begin]: ")
+    if line__ =="":
+        line__ = default
+        print("Begin selected...")
+    default = "l"
+    opcion = input("Read text by Parragraph ( p ) / line( l ) / Normal ( n ) : ")
+    if opcion == "":
+        opcion = default
+        print("Line selected ...")
     if opcion =="p" :
         _fileByParragraph(book,line__)
     elif opcion == "l" :
         _fileLineByLine(book,line__)
+    elif opcion == "n" :
+        _fileNormalReading(book,line__)
